ae5178bde8a924e50067e4de2d500cc720dbfb2d
Guilherme Siquinelli <guiseek@gmail.com>
2016-06-02 17:26:39 -0300

    DB on Mongolab

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 let config = {
-  db: 'mongodb://localhost/curso-api-fafiman'
+  // db: 'mongodb://localhost/curso-api-fafiman'
+  db: 'mongodb://dbfafiman:dbfafiman@ds023213.mlab.com:23213/api-fafiman'
 }
 
 export default config
